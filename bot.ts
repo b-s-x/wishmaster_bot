26079ad7e9fecefad0ef8f7226e65e7839cce081
b-s-x <b.s.x.mars@gmail.com>
2022-05-22 15:53:36 +0300

    bot.ts: fix

--- bot.ts
+++ bot.ts
@@ -2,16 +2,13 @@ require('dotenv').config();
 import { Context, Telegraf } from 'telegraf'
 import FilmController from './controllers/filmController';
 
-const film = new FilmController();
 const token = process.env.BOT_TOKEN;
 if (token === undefined) {
   throw new Error('BOT_TOKEN must be provided!')
 }
 
-console.log(1111111, FilmController);
-
 const bot = new Telegraf<Context>(token);
 
 bot.use(Telegraf.log());
-bot.command('films', FilmController.create());
+bot.command('films', ctx => FilmController.create(ctx));
 bot.launch();
