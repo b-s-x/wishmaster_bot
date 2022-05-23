52fd4d1293b6e8670e93a4d97f24ff87350fc6c2
b-s-x <b.s.x.mars@gmail.com>
2022-05-23 21:51:02 +0300

    bot.ts: replace bot module

--- bot.ts
+++ bot.ts
@@ -1,10 +1,11 @@
 require('dotenv').config();
 import { Context, Telegraf } from 'telegraf'
-import FilmController from './src/controllers/filmController';
 import { token } from './src/config';
+import botModule from './src/botModule';
 
 if (token === undefined) throw new Error('BOT_TOKEN must be provided!');
 const bot = new Telegraf<Context>(token);
 
-bot.command('film', FilmController.create);
-bot.launch();
+bot.catch((err: Error) => console.log(err.message, err))
+  .use(botModule)
+  .launch();
