d8f9c67e0c8ff0ab510a41690c7b53e2919c4456
b-s-x <b.s.x.mars@gmail.com>
2022-05-24 19:56:22 +0300

    @/botModule.ts: add type

--- src/botModule.ts
+++ src/botModule.ts
@@ -1,7 +1,7 @@
-import { Composer } from 'telegraf';
+import { Composer, Context } from 'telegraf';
 import FilmController from './controllers/filmController';
 
 export const bot = new Composer();
-bot.command('film', ctx => FilmController.create(ctx));
+bot.command('film', (ctx: Context) => FilmController.create(ctx));
 
 export default bot;
