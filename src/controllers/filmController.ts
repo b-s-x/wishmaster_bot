3f7a8cc0d620149225da903a916e4a0aefd7407d
b-s-x <b.s.x.mars@gmail.com>
2022-05-27 22:29:15 +0300

    @/controllers/filmController.ts: rewrite

--- src/controllers/filmController.ts
+++ src/controllers/filmController.ts
@@ -1,7 +1,13 @@
+import { Context } from 'telegraf';
+import DBController from '../db/DBcontroller';
+
 export class FilmController {
-  async create(ctx) {
-    const text = ctx.message.text.replace(/\/film/gi, '').trim();
+  async create(ctx: Context, msg: string) {
+    const text = msg.replace(/\/film/gi, '').trim();
     console.log(text);
+    await DBController.insertFilm(text);
+    console.log('success');
+    ctx.reply('success')
   }
   // async getAll(req, res) {
   // }
