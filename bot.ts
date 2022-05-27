fc91f18640bb2c0c23e6b66451518f203305821d
b-s-x <b.s.x.mars@gmail.com>
2022-05-27 22:27:20 +0300

    bot.ts: add bd

--- bot.ts
+++ bot.ts
@@ -2,9 +2,12 @@ require('dotenv').config();
 import { Context, Telegraf } from 'telegraf';
 import { token } from './src/config';
 import botModule from './src/botModule';
+import db from './src/db';
 
 if (token === undefined) throw new Error('BOT_TOKEN must be provided!');
 const bot = new Telegraf<Context>(token);
+
+db();
 
 bot.catch((err: Error) => console.log(err.message, err))
   .use(botModule)
