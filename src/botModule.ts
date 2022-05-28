34ad1d1838817d1110bcbac6bfd5d4b4f6ae1db7
b-s-x <b.s.x.mars@gmail.com>
2022-05-28 13:10:03 +0300

    @/botModule.ts: add command films

--- src/botModule.ts
+++ src/botModule.ts
@@ -2,6 +2,7 @@ import { Composer } from 'telegraf';
 import FilmController from './controllers/filmController';
 
 export const bot = new Composer();
-bot.command('film', (ctx) => FilmController.create(ctx, ctx.message.text));
+bot.command('film', ctx => FilmController.create(ctx, ctx.message.text));
+bot.command('films', ctx => FilmController.getAll(ctx));
 
 export default bot;
