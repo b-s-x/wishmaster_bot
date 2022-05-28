2842ca92feb9edfc86a17f351a5e7c01a490e4d2
b-s-x <b.s.x.mars@gmail.com>
2022-05-28 13:29:28 +0300

    @/utils/index.ts: export Data type, add return type for resultsToString

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,8 @@
-type Data = {
+export type Data = {
   id: number;
   name: string;
 };
 
-export const resultsToString = (results: Data[]) => results
+export const resultsToString = (results: Data[]): string => results
   .map(({ id, name }) => `\n${id}: ${name}`)
   .join();
