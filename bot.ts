763b6a054435bc57c7408b617e6bebddbe23b4bf
b-s-x <b.s.x.mars@gmail.com>
2022-05-28 12:32:41 +0300

    bot.ts: delete unnecessary

--- bot.ts
+++ bot.ts
@@ -2,12 +2,9 @@ require('dotenv').config();
 import { Context, Telegraf } from 'telegraf';
 import { token } from './src/config';
 import botModule from './src/botModule';
-import db from './src/db';
 
 if (token === undefined) throw new Error('BOT_TOKEN must be provided!');
 const bot = new Telegraf<Context>(token);
-
-db();
 
 bot.catch((err: Error) => console.log(err.message, err))
   .use(botModule)
